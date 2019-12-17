bd4bba65e514c9b71c9425fab468abc4559f6b94
linka39 <1203440758@qq.com>
2019-12-17 19:54:51 +0800

    头部组件以及图标设置

--- phonebook1/src/main.js
+++ phonebook1/src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import '@/assets/styles/reset.css'
 import '@/assets/styles/border.css'
+import '@/assets/styles/iconfont.css'
 
 Vue.config.productionTip = false
 
--- phonebook1/src/pages/components/Header.vue
+++ phonebook1/src/pages/components/Header.vue
@@ -1,5 +1,8 @@
 <template>
-
+  <div class="header">
+    通讯录
+    <div class="iconfont add-icon">&#xe6db;</div>
+  </div>
 </template>
 
 <script>
@@ -9,9 +12,19 @@
 </script>
 
 <style lang="stylus" scoped>
+  @import "~@/assets/styles/varibles.styl"
   .header
-    position relative;
-    overflow hidden;
-    height 0.86rem
-
+    position: relative;
+    overflow: hidden;
+    background: $bgColor;
+    font-size: .48rem;
+    font-weight: 400;
+    text-align: center;
+    height: .86rem;
+    line-height: .86rem;
+    .add-icon
+      position fixed
+      top 0rem
+      right .45rem
+      font-size .5rem
 </style>
