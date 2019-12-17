a9587626310069fd407448c2afe9ced84a6dea90
linka39 <1203440758@qq.com>
2019-12-17 17:07:49 +0800

    前端通讯录根据字母表滑动实现滚动

--- phonebook1/src/pages/components/Alphabet.vue
+++ phonebook1/src/pages/components/Alphabet.vue
@@ -2,13 +2,25 @@
     <ul class="list">
       <li class="item"
           @click="handleLetterClick"
-          v-for="(item,key) of letters">{{item}}</li>
+          @touchstart="handleTouchStart"
+          @touchmove="handleTouchMove"
+          @touchend="handleTouchEnd"
+          v-for="(item,key) of letters"
+          :key="item"
+          :ref="item">{{item}}</li>
     </ul>
 </template>
 
 <script>
     export default {
         name: "Alphabet",
+        data(){
+          return{
+            touchStatus:false,
+            startY:0,
+            timer:''
+          }
+        },
         props:{
           phoneBooks:Object
         },
@@ -27,8 +39,39 @@
             console.log(e.target.innerText);
             //子组件向父组件传值
             this.$emit("change",e.target.innerText);
-          }
-        }
+          },
+          handleTouchStart(){
+            this.touchStatus=true;
+            console.log("start");
+          },
+          handleTouchMove(e){
+            console.log("move");
+            //用setTimeout来进行性能优化
+            if(this.timer){
+              clearTimeout(this.timer);
+            }
+            this.timer=setTimeout(()=>{
+              if(this.touchStatus){
+                //offsetTop为dom元素距顶部高度
+                const touchY=e.touches[0].clientY-79;
+                console.log("startY:" + this.startY);
+                //Math.floor(a) 对a进行向下取整
+                const index=Math.floor((touchY-this.startY)/20);
+                if(index >=0 && index<this.letters.length){
+                  this.$emit("change",this.letters[index]);
+                }
+              }
+            },14)
+          },
+          handleTouchEnd(){
+            this.touchStatus=false;
+            console.log("end");
+          },
+        },
+      //dom挂载完，并且数据传输过来
+      updated() {
+        this.startY=this.$refs['A'][0].offsetTop;
+      }
     }
 </script>
 
